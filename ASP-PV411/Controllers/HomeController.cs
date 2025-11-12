45f259667093548d201065c21c31126095fae61a
Ancie <173624031+Ancie13@users.noreply.github.com>
2025-11-12 15:40:13 +0200

    Add project files.

---
+++ ASP-PV411/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ASP_PV411.Models;
+
+namespace ASP_PV411.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Intro()
+    {
+        return View();
+    }
+
+    public IActionResult Razor()
+    {
+        return View();
+    }
+
+    public IActionResult History()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
